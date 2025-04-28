3f7cebacb2b19bdba8cc00d9e9db8377e015ffca
Kaho Otani <a1oe1ha@gmail.com>
2025-04-28 11:47:53 -0700

    contact form CSS added

--- confirm.php
+++ confirm.php
@@ -1,6 +1,4 @@
 <?php ini_set('display_errors', 1);
-echo "Hello";
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $name = $_POST["name"];
   $email = $_POST["email"];
@@ -17,6 +15,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./contact.css">
+  <link href="https://fonts.googleapis.com/css2?family=Give+You+Glory&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <title>Contact Form Confirmation</title>
 </head>
 
@@ -25,17 +25,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <p></p>
   <table>
     <tr>
+      <td class="title">Name</td>
       <td><?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?></td>
     </tr>
     <tr>
+      <td class="title">Email</td>
       <td><?php echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8') ?></td>
     </tr>
     <tr>
+      <td class="title">Subject</td>
       <td><?php echo htmlspecialchars($subject, ENT_QUOTES, 'UTF-8') ?></td>
     </tr>
-    <tr>
+    <tr">
+      <td class="title">Message</td>
       <td><?php echo htmlspecialchars($msg, ENT_QUOTES, 'UTF-8') ?></td>
-    </tr>
+      </tr>
   </table>
 
   <form method="post" action="send.php">
@@ -43,7 +47,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <input type="hidden" name="email" value="<?php echo $email; ?>">
     <input type="hidden" name="subject" value="<?php echo $subject; ?>">
     <input type="hidden" name="msg" value="<?php echo $msg; ?>">
-    <input type="submit" value="Submit">
+    <button type="button" onclick="history.back()" class="button">Back</button>
+    <input type="submit" value="Submit" class="button">
   </form>
 </body>
 
--- send.php
+++ send.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $body = "Name: $name \n Email: $email \n Subject: $subject \n Message: $msg";
 
   if (mail($to, $subject, $body, $headers)) {
-    $message = "Your request was sent!";
+    $message = "Your message was sent!";
   } else {
     $code = $e->getCode();
     $error = $e->getMessage();
@@ -27,13 +27,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./contact.css">
+  <link href="https://fonts.googleapis.com/css2?family=Give+You+Glory&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <title>Submittion Complete</title>
 </head>
 
 <body>
   <h1>Thank you for reaching out!</h1>
   <p><?php echo $message; ?></p>
-  <a href="index.html">Back to Home</a>
+  <a href="index.html" class="button">Back to Home</a>
 </body>
 
 </html>
